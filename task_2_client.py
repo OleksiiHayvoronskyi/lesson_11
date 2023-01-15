3d8d5d17973ffd5a9e55d1dd1e36f54f7d64a96e
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-01-15 23:09:43 +0200

    Modified code

--- task_2_client.py
+++ task_2_client.py
@@ -14,18 +14,45 @@ address = ('localhost', 53132)
 max_size = 1024
 
 print('Client has been started at', datetime.now(), '\n')
+name = input(str('Enter your name: '))
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Trying to connect to', address)
 client.connect(address)
-client.sendall(b'Hey, Server!'.upper())
-data = client.recv(max_size)
-print('At', datetime.now(), 'Server answered:', data)
 
-print('====================')
+client.sendall(name.encode())
+c_name = client.recv(max_size)
+c_name = c_name.decode()
+print(c_name, 'has joined to the chat room')
+print('Enter [break] to exit chat room]\n')
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(address)
-client.sendall(b'I want to get info, Server!'.upper())
-data = client.recv(max_size)
-print('At', datetime.now(), 'Server answered:', data)
+while True:
+    message = client.recv(max_size)
+    message = message.decode()
+    print(c_name, ":", message)
+    message = input(str("Me: "))
+    if message == 'break':
+        message = "Left chat room!"
+        client.send(message.encode())
+        print("\n")
+        break
+    client.send(message.encode())
+
+
+
+
+# print('Client has been started at', datetime.now(), '\n')
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect(address)
+# client.sendall(b'Hey, Server!'.upper())
+# data = client.recv(max_size)
+# print('At', datetime.now(), 'Server answered:', data)
+#
+# print('====================')
+#
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect(address)
+# client.sendall(b'I want to get info, Server!'.upper())
+# data = client.recv(max_size)
+# print('At', datetime.now(), 'Server answered:', data)
 
 client.close()
--- task_2_server.py
+++ task_2_server.py
@@ -4,6 +4,7 @@
 from datetime import datetime
 import socket
 
+
 # СЕРВЕР
 
 print('--- Task 2. TCP Server ---')
@@ -11,23 +12,49 @@ print('--- Task 2. TCP Server ---')
 address = ('localhost', 53132)
 max_size = 1024
 
-print('Server has been started at', datetime.now())
+print('Welcome to the ChatBox')
 print('Server is waiting on a client...\n')
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(address)
+print(address)
+name = input(str('Enter your name: '))
+
 server.listen(5)
 
-while True:
-    conn, addr = server.accept()
-    with conn:
-        print('Connected with:', addr)
-        data = str(conn.recv(max_size), encoding='utf_8')
-        if not data:
-            break
-        print(f'Client {addr} said:', data)
-        # Повідомлення сервера.
-        server_message = data
-        print('====================')
-        conn.sendall(server_message.encode())
-        conn.close()
+print("\nWaiting for incoming connections...\n")
+conn, addr = server.accept()
+print("Received connection from ", addr[0], "(", addr[1], ")\n")
 
+s_name = conn.recv(1024)
+s_name = s_name.decode()
+print(s_name, 'has joined to the chat room')
+print('Enter [break] to exit chat room]\n')
+
+conn.send(name.encode())
+
+while True:
+    message = input(str("Me: "))
+    if message == 'break':
+        message = "Left chat room!"
+        conn.send(message.encode())
+        print("\n")
+        break
+    conn.send(message.encode())
+    message = conn.recv(1024)
+    message = message.decode()
+    print(s_name, ":", message)
+
+# while True:
+#     conn, addr = server.accept()
+#     with conn:
+#         print('Connected with:', addr)
+#         data = str(conn.recv(max_size), encoding='utf_8')
+#         if not data:
+#             break
+#         print(f'Client {addr} said:', data)
+#         # Повідомлення сервера.
+#         server_message = data
+#         print('====================')
+#         conn.sendall(server_message.encode())
+#         conn.close()
+#
